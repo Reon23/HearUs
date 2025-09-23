ea757cbb98d2f1a387055e8bbd58421ccb927f67
Knight-eGithub <githubknight@gmail.com>
2025-09-23 06:31:38 -0700

    Working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,41 @@
 import React from "react";
-import FloatingButton from "./components/FloatingButton";
-import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// import Home from "@/pages/Home"
+// import Login from "@/pages/Login"
+// import Register from "@/pages/Register"
+// import FileComplaint from "../pages/FileComplaint"
+import DashboardLayout from "@/layouts/DashboardLayout"
+import DashboardHome from "@/pages/dashboard/Home"
+import { useState } from "react";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/Home";
-import FileComplaint from "./pages/FileComplaint";
+// import Reports from "@/pages/dashboard/Reports"
+// import Team from "@/pages/dashboard/Team"
 
-// Inner component where hooks can be used
-function AppWrapper() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleClick = () => {
-    if (location.pathname === "/complaint") {
-      navigate("/"); // go home if already on map
-    } else {
-      navigate("/complaint"); // otherwise go to map
-    }
-  };
-
-  return (
-    <>
-      <Navbar />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/complaint" element={<FileComplaint />} />
-          <Route path="/LoginPage" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-
-        <FloatingButton onClick={handleClick} />
-      </div>
-    </>
-  );
-}
-
-// Outer wrapper to provide Router
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   return (
     <Router>
-      <AppWrapper />
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/complaint" element={<FileComplaint />} /> */}
+
+        {/* Dashboard layout route */}
+        {isAuthenticated && 
+          <Route path="/" element={<DashboardLayout />}>
+            {/* Nested routes render inside <Outlet /> */}
+            <Route index element={<Home />} />
+            <Route element={<DashboardHome />} />
+            {/* <Route path="complaints" element={<Complaints />} /> */}
+            {/* <Route path="team" element={<Team />} /> */}
+          </Route>
+        }
+      </Routes>
     </Router>
   );
 }
--- client/src/pages/FileComplaint.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FileComplaint = () => {
-  return (
-    <div>FileComplaint</div>
-  )
-}
-
-export default FileComplaint
